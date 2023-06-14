94956b5b797adadbc5bfdb7c2b8693a568485a09
Chirag Mathur <54184768+chiragmathur03@users.noreply.github.com>
2023-06-14 23:19:37 +0530

    Add Links to Footer

--- src/app/MyComponents/footer/footer.component.ts
+++ src/app/MyComponents/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+  constructor(private router: Router) { }
+  navigateToTermsConditions() {
+    const bodyElement = document.querySelector('body');
+    if (bodyElement) {
+      bodyElement.classList.add('fade-out');
+      setTimeout(() => {
+        window.location.href = '/termsconditions';
+      }, 150); // Adjust the delay (in milliseconds) to match the duration of your fade-out animation
+    }
+  }
+  navigateToPrivacy() {
+    const bodyElement = document.querySelector('body');
+    if (bodyElement) {
+      bodyElement.classList.add('fade-out');
+      setTimeout(() => {
+        window.location.href = '/privacy';
+      }, 150); // Adjust the delay (in milliseconds) to match the duration of your fade-out animation
+    }
+  }
+  navigateToContact() {
+    const bodyElement = document.querySelector('body');
+    if (bodyElement) {
+      bodyElement.classList.add('fade-out');
+      setTimeout(() => {
+        window.location.href = '/contact';
+      }, 150); // Adjust the delay (in milliseconds) to match the duration of your fade-out animation
+    }
+  }
+  navigateToFAQ() {
+    const bodyElement = document.querySelector('body');
+    if (bodyElement) {
+      bodyElement.classList.add('fade-out');
+      setTimeout(() => {
+        window.location.href = '/faq';
+      }, 150); // Adjust the delay (in milliseconds) to match the duration of your fade-out animation
+    }
+  }
+  navigateToWorks() {
+    const bodyElement = document.querySelector('body');
+    if (bodyElement) {
+      bodyElement.classList.add('fade-out');
+      setTimeout(() => {
+        window.location.href = '/';
+      }, 150); // Adjust the delay (in milliseconds) to match the duration of your fade-out animation
+    }
+  }
+  navigateToServices() {
+    const bodyElement = document.querySelector('body');
+    if (bodyElement) {
+      bodyElement.classList.add('fade-out');
+      setTimeout(() => {
+        window.location.href = '/services';
+      }, 150); // Adjust the delay (in milliseconds) to match the duration of your fade-out animation
+    }
+  }
+  navigateToAbout() {
+    const bodyElement = document.querySelector('body');
+    if (bodyElement) {
+      bodyElement.classList.add('fade-out');
+      setTimeout(() => {
+        window.location.href = '/services';
+      }, 150); // Adjust the delay (in milliseconds) to match the duration of your fade-out animation
+    }
+  }
 }
+
